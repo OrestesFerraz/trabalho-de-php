5e57cb5375d4a84fbd0cef2f84a5209097891085
OrestesFerraz <148365065+OrestesFerraz@users.noreply.github.com>
2024-05-15 23:25:26 -0300

    Add files via upload

---
+++ destinopost.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+    body {
+        font-family: sans;
+        background-color: black;
+        color: white;
+        font-size: 3vw;
+        text-align: center;
+    }
+</style>
+</head>
+<body>
+    <h3>
+    <?php
+        print_r($_POST);
+    ?>
+    </h3>
+    
+</body>
+</html>
